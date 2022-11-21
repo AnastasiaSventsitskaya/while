15b8f82517827201aab76e6e5f806d885241f6e5
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-21 15:55:33 +0400

    ex Masha

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Write username");
+String username = Console.ReadLine();
+
+if
+(username.ToLower() == "masha")
+{
+    Console.WriteLine ("YEEES, This is Masha");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
